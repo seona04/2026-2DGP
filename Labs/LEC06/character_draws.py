e2613d2c9330b3162995b53358b0f91bfd2cbb9e
Woon2 <73771162+Woon-2@users.noreply.github.com>
2026-09-22 16:43:22 +0900

    캔버스 열기 구현

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,3 +1,24 @@
 # 실습 과제 진행
+from pico2d import *
+
+open_canvas(800, 600)
+
+
+
+def move_circle():
+    print("CIRCLE")
+    pass
+
+def move_rectangle():
+    print("RECTANGLE")
+    pass
+
+def move_triangle():
+    print("TRIANGLE")
+    pass
+
 while True:
+    move_circle()
+    move_rectangle()
+    move_triangle()
     pass
